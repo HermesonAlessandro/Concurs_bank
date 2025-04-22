3faf07c941ddbdb98a8ff7c99c59be6955ef5a75
@HermesonAlessandro <105324312+HermesonAlessandro@users.noreply.github.com>
2025-04-22 15:33:59 -0300

    Alterações
    Alterações

---
+++ bd/concurs_bank.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Tempo de geração: 22-Abr-2025 às 20:33
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `concurs_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estudante`
+--
+
+CREATE TABLE `estudante` (
+  `cpf` varchar(14) NOT NULL,
+  `nome_completo` varchar(100) NOT NULL,
+  `idade` int(11) NOT NULL,
+  `sexo` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `estudante`
+--
+ALTER TABLE `estudante`
+  ADD PRIMARY KEY (`cpf`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
