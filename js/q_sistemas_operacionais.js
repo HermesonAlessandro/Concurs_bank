56d50b156254e6453bb50f33b2aaca5758643a62
Igor Emanuel <emanueligor915@gmail.com>
2025-05-24 02:08:38 -0300

    q_sistemas_operacionais

--- js/q_sistemas_operacionais.js
+++ js/q_sistemas_operacionais.js
@@ -1,202 +1,203 @@
 // Array contendo as questões do quiz
 const questoes = [
         {
-            id: "", 
-            pergunta: "",
+            id: "questao1", 
+            pergunta: " (CESPE - 2022 - TCU) Acerca dos sistemas operacionais, julgue o item. Um dos principais objetivos de um sistema operacional é fornecer uma interface entre o hardware e o usuário, além de gerenciar recursos como memória, processador e dispositivos de entrada esaída.",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Verdadeiro",
+                B: "Falso",
+                C: "Depende do sistema operacional",
+                D: "Apenas se for um sistema multiusuário",
+                E: "Apenas em sistemas de tempo real"
             },
-            correta: "",
+            correta: "A",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "O SO fornece uma interface entre hardware e usuário, além de gerenciar  recursos como memória, CPU e E/S. As demais estão incorretas pelos motivos descritos",
+                B: "Um sistema operacional realmente faz isso, então essa alternativa está incorreta.",
+                C: "Todos os sistemas operacionais modernos possuem essa função básica.",
+                D: "Essa função é independente do número de usuários.",
+                E: "Sistemas de tempo real têm requisitos específicos, mas todos os sistemas operacionais fazem esse gerenciamento."
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao2", 
+            pergunta: "(FCC - 2020 - TRT 2ª Região) No gerenciamento de processos, qual é o estado que indica que um processo está esperando por algum evento, como a conclusão de uma operação de E/S?",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Pronto",
+                B: "Executando",
+                C: "Bloqueado",
+                D: "Finalizado",
+                E: "Suspenso"
             },
-            correta: "",
+            correta: "C",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Indica que o processo está aguardando uso da CPU, não de E/S.",
+                B: "O processo está em execução, não esperando.",
+                C: " Estado Bloqueado significa que o processo está esperando por um evento externo. As demais representam outros estados do ciclo de vida do processo.",
+                D: " O processo já terminou sua execução.",
+                E: " Indica que o processo foi retirado da memória, não que está esperando evento."
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao3", 
+            pergunta: " (CESGRANRIO - 2021 - Petrobras) O sistema operacional utiliza uma área especial chamada de memória virtual. Assinale a alternativacorreta sobre esse conceito",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Impede o uso de disco como memória",
+                B: "Permite executar processos maiores que a memória física",
+                C: "Está presente apenas em SOs de tempo real",
+                D: "É um tipo de cache",
+                E: "Substitui a RAM"
             },
-            correta: "",
+            correta: "B",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Pelo contrário, a memória virtual usa o disco como extensão da RAM.",
+                B: "Memória virtual permite que processos maiores que a RAM sejam executados, usando parte do disco como extensão",
+                C: "Está presente em vários tipos de SOs, especialmente os de propósito geral.",
+                D: "Cache é memória rápida entre o processador e a RAM. Memória virtual é outra coisa.",
+                E: "Ela complementa, não substitui a RAM"
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao4", 
+            pergunta: " (IBFC - 2019 - TJ-MG) Qual técnica de gerenciamento de memória divide a memória em blocos fixos?",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Paginação",
+                B: "Segmentação",
+                C: "Swapping",
+                D: "Multiprogramação",
+                E: "Cacheamento"
             },
-            correta: "",
+            correta: "A",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "A paginação divide a memória em blocos fixos chamados páginas. As demais técnicas têm outros propósitos.",
+                B: "Divide a memória com base em lógica do programa, com blocos variáveis",
+                C: "Move processos entre memória e disco, não divide a memória em blocos.",
+                D: "Refere-se à execução de múltiplos processos, não à divisão de memória",
+                E: "Relaciona-se com armazenamento temporário de dados, não com divisão da RAM"
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao5", 
+            pergunta: " (FGV - 2021 - SEFAZ-RJ)Em relação aos sistemas de arquivos, qual das opções abaixo representa um sistema de arquivos utilizado em sistemas Linux?",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "NTFS",
+                B: "FAT32",
+                C: "ext4",
+                D: "HFS+",
+                E: "APFS"
             },
-            correta: "",
+            correta: "C",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Sistema de arquivos padrão do Windows.",
+                B: "Sistema antigo usado em dispositivos removíveis e Windows.",
+                C: " O ext4 é um sistema de arquivos padrão no Linux. As demais são de Windows ou MacOS",
+                D: "Usado por sistemas macOS antigos.",
+                E: "Sistema de arquivos usado por versões modernas do macOS."
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao6", 
+            pergunta: " (CESPE - 2022 - Polícia Federal) O comando chmod no Linux é utilizado para:",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Gerenciar usuários",
+                B: "Alterar permissões de arquivos",
+                C: "Listar processos",
+                D: "Monitorar CPU",
+                E: "Instalar pacotes"
             },
-            correta: "",
+            correta: "B",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Isso seria feito com useradd, usermod, entre outros",
+                B: "chmod altera permissões de arquivos e diretórios no Linux.",
+                C: "Isso é feito com ps, top, etc",
+                D: "Comandos como top ou htop são usados para isso",
+                E: " Usam-se apt, yum, dnf etc., dependendo da distribuição"
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao7", 
+            pergunta: " (IBFC - 2023 - PC-SP) No Windows, qual funcionalidade permite criar um ponto de restauração do sistema?",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Gerenciador de Tarefas",
+                B: "Configurações de Rede",
+                C: "Restauração do Sistema",
+                D: "Gerenciamento de Disco",
+                E: "Prompt de Comando"
             },
-            correta: "",
+            correta: "C",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Serve para visualizar e encerrar processos",
+                B: "Gerencia conexões de rede.",
+                C: "A Restauração do Sistema permite criar pontos para reverter alterações no Windows.",
+                D: "Serve para particionar e formatar discos.",
+                E: "Interface de linha de comando; não cria pontos de restauração diretamente."
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao8", 
+            pergunta: " (FCC - 2020 - TRF 3ª Região) A técnica que permite que vários processos compartilhem a CPU alternadamente é chamada de:",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Pipeline",
+                B: "Multithreading",
+                C: "Multiprogramação",
+                D: "Multitarefa",
+                E: "Swapping"
             },
-            correta: "",
+            correta: "D",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Relacionado ao paralelismo dentro da CPU.",
+                B: "Refere-se à execução simultânea de múltiplas threads.",
+                C: "Refere-se à presença de múltiplos programas na memória, não à alternância de execução.",
+                D: "A multitarefa permite alternar rapidamente entre processos, simulando execução simultânea",
+                E: "Técnica de troca de processos entre memória e disco."
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao9", 
+            pergunta: "(CESPE - 2021 - CGU)Em sistemas operacionais, deadlock ocorre quando:",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Um processo é finalizado abruptamente",
+                B: "A CPU superaquece",
+                C: "Processos ficam esperando indefinidamente recursos ocupados",
+                D: "Processos ficam esperando indefinidamente recursos ocupados",
+                E: " A memória RAM chega a 100%
+"
             },
-            correta: "",
+            correta: "C",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Isso é uma falha, não deadlock.",
+                B: "Problema físico, não lógico.",
+                C: "Deadlock ocorre quando processos ficam aguardando indefinidamente recursos ocupados entre eles.",
+                D: "Também é uma falha de hardware, não deadlock.",
+                E: "Pode causar lentidão, mas não configura deadlock."
             }
         },
 
         {
-            id: "", 
-            pergunta: "",
+            id: "questao10", 
+            pergunta: "(VUNESP - 2022 - Polícia Civil) Em relação ao ciclo de vida de um processo, quando ocorre o estado Pronto (Ready)?",
             alternativas:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Aguardando E/S",
+                B: "Usando a CPU",
+                C: "Aguardando ser escalonado",
+                D: "Processo terminado",
+                E: "Processo movido para o disco"
             },
-            correta: "",
+            correta: "C",
             explicacoes:{
-                A: "",
-                B: "",
-                C: "",
-                D: "",
-                E: ""
+                A: "Isso é o estado Bloqueado",
+                B: "sso é o estado Executando",
+                C: "O estado Pronto indica que o processo está pronto para ser executado, aguardando o processador",
+                D: "Isso é o estado Finalizado",
+                E: "Não é um estado padrão no ciclo de vida."
             }
         }
     ];
